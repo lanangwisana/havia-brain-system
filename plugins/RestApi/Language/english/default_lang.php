c844c31f0ece8b032079a7f860273ff3d758de12
lanangwisana <lanangwisana@gmail.com>
2026-03-01 00:33:22 +0700

    feat(plugin): add RestApi language files

---
+++ plugins/RestApi/Language/english/default_lang.php
@@ -0,0 +1,311 @@
+<?php
+
+/* NOTE: DO NOT CHANGE THIS FILE. IF YOU WANT TO UPDATE THE LANGUAGE THEN COPY THIS FILE TO custom_lang.php AND UPDATE THERE */
+
+/* language locale */
+$lang["language_locale"]      = "en"; //locale code
+$lang["language_locale_long"] = "en-US"; //long locale code
+
+$lang["api"]                      = "Api";
+$lang["rest_api_main_menu"]       = "API";
+$lang["api_user"]                 = "Email";
+$lang["api_name"]                 = "Name";
+$lang["token"]                    = "Token";
+$lang['new_user']                 = "New User";
+$lang['enter_username']           = "Enter User Name";
+$lang['enter_useremail']          = "Enter User Email";
+$lang['expiration_date']          = "Expiration date";
+$lang["api"]                      = "API management";
+$lang["company_name_is_required"] = "Company name is required";
+$lang['valid_company_name']       = "Company name should only contains alphabet characters";
+$lang['valid_phone']              = "Phone should only contains numeric value";
+$lang['valid_website']            = "Website should contains valid url";
+$lang['owner_is_required']        = "Owner ID is required";
+$lang['valid_owner_id']           = "Owner ID should only contains numeric value";
+$lang['owner_id_invalid']         = "Invalid owner ID";
+$lang['lead_status_is_required']  = "Lead status is required";
+$lang['valid_lead_status_id']     = "Lead status ID should only contains numeric value";
+$lang['lead_source_is_required']  = "Lead source is required";
+$lang['valid_lead_source_id']     = "Lead source ID should only contains numeric value";
+$lang['lead_status_id_invalid']   = "Invalid lead status ID";
+$lang['lead_source_id_invalid']   = "Invalid lead source ID";
+$lang['project_title_required']   = "Project title is required";
+$lang['client_id_required']       = "Client ID is required";
+$lang['client_id_invalid']        = "Invalid Client ID";
+$lang['start_data_invalid']       = "Start date should contain a valid date";
+$lang['deadline_invalid']         = "Deadline should contain a valid date";
+$lang['price_is_invalid']         = "Price should contain a valid value";
+$lang['label_is_invalid']         = "Invalid Label ID";
+$lang['no_data_were_found']       = "No data were found";
+$lang['client_added_success']     = "Client add successful.";
+$lang['client_added_fail']        = "Client add fail.";
+$lang['client_delete_success']    = "Client Deleted Successfuly.";
+$lang['client_delete_fail']       = "Client Delete Fail.";
+$lang['project_add_success']      = "Project add successful.";
+$lang['project_add_fail']         = "Project add fail.";
+$lang['invalid_project_id']       = "Invalid Project ID";
+$lang['project_delete_success']   = "Project Deleted Successfuly.";
+$lang['project_delete_fail']      = "Project Delete Fail.";
+$lang['lead_add_success']         = "Lead add successful.";
+$lang['lead_add_fail']            = "Lead add fail.";
+$lang['lead_delete_success']      = "Lead Deleted Successfuly.";
+$lang['lead_delete_fail']         = "Lead Delete Fail.";
+$lang['invalid_lead_id']          = "Invalid Lead ID";
+$lang['client_update_success']    = "Client Update Successful.";
+$lang['client_update_fail']       = "Client Update Fail.";
+$lang['lead_update_success']      = "Lead Update Successful.";
+$lang['lead_update_fail']         = "Lead Update Fail.";
+$lang['project_update_success']   = "Project Update Successful.";
+$lang['project_update_fail']      = "Project Update Fail.";
+$lang['invalid_group_id']         = "Invalid Client group id.";
+$lang['ticket_delete_success']    = "Ticket Deleted Successfuly.";
+$lang['ticket_delete_fail']       = "Ticket Delete Fail.";
+$lang['ticket_title_required']    = "Ticket title is required";
+$lang['requested_by_id_required'] = "Request by id is required field";
+$lang['requested_by_id_invalid']  = "Invalid Request by id";
+$lang['description_is_required']  = "Description is required";
+$lang['assigned_to_required']	  = "Assigned to is required";
+$lang['invalid_assigned_to'] 	  = "Invalid Assigned to";
+$lang['ticket_type_required']	  = "Ticket type is required";
+$lang['ticket_type_invalid']	  = "Invalid Ticket type";
+$lang['ticket_add_success']		  = "Ticket add successful.";
+$lang['ticket_add_fail']	 	  = "Ticket add fail.";
+$lang['invoice_id_invalid']	 	  = "Invalid Invoice Id";
+$lang['invoice_delete_success']	  = "Invoice Delete Successfuly.";
+$lang['invoice_delete_fail']	  = "Invoice Delete Fail.";
+$lang['ticket_id_invalid']		  = "Invalid Ticket ID";
+$lang['ticket_update_success']	  = "Ticket update successful.";
+$lang['ticket_update_fail']	 	  = "Ticket update fail.";
+$lang['invoice_date_required']	  = "Invoice date is required";
+$lang['invoice_date_invalid']	  = "Invalid Invoice date should be formatted in Y-m-d";
+$lang['invoice_due_date_required']= "Invoice due date is required";
+$lang['invoice_due_date_invalid'] = "Invalid Invoice due date should be formatted in Y-m-d";
+$lang['invalid_tax_id'] 		  = "Invalid Tax ID";
+$lang['invalid_tax2_id'] 		  = "Invalid Tax 2 ID";
+$lang['invalid_tax3_id'] 		  = "Invalid Tax 3 ID";
+$lang['allowed_recurring_values'] = "Only 0 and 1 Allowed in Recurring";
+$lang['repeat_every_required']	  = "Repeat every is required";
+$lang['invalid_repeat_every']	  = "Invalid repeat every";
+$lang['greater_than_repeat_every']= "Repeat every should greater than 0";
+$lang['repeat_type_required']	  = "Repeat type is required";
+$lang['repeat_type_allowed']	  = "Repeat type only allows days,weeks,months,years";
+$lang['no_of_cycles_required']	  = "No of cycles is required";
+$lang['invalid_no_of_cycles']	  = "Invalid no of cycles";
+$lang['greater_than_no_of_cycles']= "No of cycle should not be a 0";
+$lang['invoice_add_success']	  = "Invoice add successful";
+$lang['invoice_add_fail']	  	  = "Invoice add fail";
+$lang['invoice_update_success']	  = "Invoice update successful";
+$lang['invoice_update_fail']	  = "Invoice update fail";
+
+// Comment (Project/Ticket)
+$lang['comment_add_success']       = "Comment added successfully.";
+$lang['comment_add_fail']         = "Failed to add comment. Ensure project_id and description are sent in the request body (JSON or form-urlencoded).";
+$lang['comment_update_success']   = "Comment updated successfully.";
+$lang['comment_update_fail']      = "Failed to update comment.";
+$lang['comment_delete_success']   = "Comment deleted successfully.";
+$lang['comment_delete_fail']      = "Failed to delete comment.";
+$lang['invalid_comment_id']       = "Invalid comment ID.";
+
+// Contract
+$lang['contract_add_success']      = "Contract added successfully.";
+$lang['contract_add_fail']         = "Failed to add contract.";
+$lang['contract_update_success']   = "Contract updated successfully.";
+$lang['contract_update_fail']      = "Failed to update contract.";
+$lang['contract_delete_success']  = "Contract deleted successfully.";
+$lang['contract_delete_fail']     = "Failed to delete contract.";
+$lang['invalid_contract_id']      = "Invalid contract ID.";
+
+// Event
+$lang['event_add_success']         = "Event added successfully.";
+$lang['event_add_fail']            = "Failed to add event.";
+$lang['event_update_success']     = "Event updated successfully.";
+$lang['event_update_fail']        = "Failed to update event.";
+$lang['event_delete_success']     = "Event deleted successfully.";
+$lang['event_delete_fail']        = "Failed to delete event.";
+$lang['invalid_event_id']         = "Invalid event ID.";
+
+// Proposal
+$lang['proposal_add_success']     = "Proposal added successfully.";
+$lang['proposal_add_fail']        = "Failed to add proposal.";
+$lang['proposal_update_success']  = "Proposal updated successfully.";
+$lang['proposal_update_fail']     = "Failed to update proposal.";
+$lang['proposal_delete_success']  = "Proposal deleted successfully.";
+$lang['proposal_delete_fail']     = "Failed to delete proposal.";
+$lang['invalid_proposal_id']      = "Invalid proposal ID.";
+$lang['proposal_date_required']   = "Proposal date is required.";
+$lang['invalid_proposal_date']    = "Invalid proposal date.";
+$lang['valid_until_required']     = "Valid until date is required.";
+$lang['invalid_valid_until']      = "Invalid valid until date.";
+
+// Order
+$lang['order_add_success']        = "Order added successfully.";
+$lang['order_add_fail']           = "Failed to add order.";
+$lang['order_update_success']     = "Order updated successfully.";
+$lang['order_update_fail']       = "Failed to update order.";
+$lang['order_delete_success']     = "Order deleted successfully.";
+$lang['order_delete_fail']        = "Failed to delete order.";
+$lang['invalid_order_id']         = "Invalid order ID.";
+
+// ToDo
+$lang['todo_add_success']         = "ToDo added successfully.";
+$lang['todo_add_fail']            = "Failed to add ToDo.";
+$lang['todo_update_success']     = "ToDo updated successfully.";
+$lang['todo_update_fail']        = "Failed to update ToDo.";
+$lang['todo_delete_success']     = "ToDo deleted successfully.";
+$lang['todo_delete_fail']        = "Failed to delete ToDo.";
+$lang['invalid_todo_id']          = "Invalid ToDo ID.";
+
+// Notification
+$lang['notification_add_success'] = "Notification added successfully.";
+$lang['notification_add_fail']    = "Failed to add notification.";
+$lang['notification_update_success'] = "Notification updated successfully.";
+$lang['notification_update_fail'] = "Failed to update notification.";
+$lang['notification_delete_success'] = "Notification deleted successfully.";
+$lang['notification_delete_fail'] = "Failed to delete notification.";
+$lang['invalid_notification_id']  = "Invalid notification ID.";
+
+// Estimate
+$lang['estimate_add_success']     = "Estimate added successfully.";
+$lang['estimate_add_fail']        = "Failed to add estimate.";
+$lang['estimate_update_success']  = "Estimate updated successfully.";
+$lang['estimate_update_fail']     = "Failed to update estimate.";
+$lang['estimate_delete_success']  = "Estimate deleted successfully.";
+$lang['estimate_delete_fail']     = "Failed to delete estimate.";
+$lang['invalid_estimate_id']      = "Invalid estimate ID.";
+$lang['estimate_date_required']   = "Estimate date is required.";
+$lang['invalid_estimate_date']    = "Invalid estimate date.";
+$lang['invalid_estimate_status']  = "Invalid estimate status.";
+$lang['invalid_discount_amount']  = "Invalid discount amount.";
+$lang['invalid_discount_type']    = "Invalid discount type.";
+
+// Milestone
+$lang['milestone_add_success']    = "Milestone added successfully.";
+$lang['milestone_add_fail']       = "Failed to add milestone.";
+$lang['milestone_update_success'] = "Milestone updated successfully.";
+$lang['milestone_update_fail']    = "Failed to update milestone.";
+$lang['milestone_delete_success'] = "Milestone deleted successfully.";
+$lang['milestone_delete_fail']    = "Failed to delete milestone.";
+$lang['invalid_milestone_id']     = "Invalid milestone ID.";
+
+// Message
+$lang['message_add_success']      = "Message added successfully.";
+$lang['message_add_fail']         = "Failed to add message.";
+$lang['message_update_success']   = "Message updated successfully.";
+$lang['message_update_fail']      = "Failed to update message.";
+$lang['message_delete_success']   = "Message deleted successfully.";
+$lang['message_delete_fail']      = "Failed to delete message.";
+$lang['invalid_message_id']       = "Invalid message ID.";
+
+// Note
+$lang['note_add_success']         = "Note added successfully.";
+$lang['note_add_fail']            = "Failed to add note.";
+$lang['note_update_success']     = "Note updated successfully.";
+$lang['note_update_fail']        = "Failed to update note.";
+$lang['note_delete_success']     = "Note deleted successfully.";
+$lang['note_delete_fail']        = "Failed to delete note.";
+$lang['invalid_note_id']          = "Invalid note ID.";
+
+// Leave Application
+$lang['leave_application_add_success'] = "Leave application added successfully.";
+$lang['leave_application_add_fail']    = "Failed to add leave application.";
+$lang['leave_application_update_success'] = "Leave application updated successfully.";
+$lang['leave_application_update_fail'] = "Failed to update leave application.";
+$lang['leave_application_delete_success'] = "Leave application deleted successfully.";
+$lang['leave_application_delete_fail'] = "Failed to delete leave application.";
+$lang['invalid_leave_application_id']   = "Invalid leave application ID.";
+
+// Invoice Payment
+$lang['payment_add_success']      = "Payment added successfully.";
+$lang['payment_add_fail']         = "Failed to add payment.";
+$lang['payment_update_success']   = "Payment updated successfully.";
+$lang['payment_update_fail']      = "Failed to update payment.";
+$lang['payment_delete_success']   = "Payment deleted successfully.";
+$lang['payment_delete_fail']      = "Failed to delete payment.";
+$lang['invalid_payment_id']       = "Invalid payment ID.";
+
+// Attendance
+$lang['attendance_add_success']   = "Attendance added successfully.";
+$lang['attendance_add_fail']      = "Failed to add attendance.";
+$lang['attendance_update_success'] = "Attendance updated successfully.";
+$lang['attendance_update_fail']   = "Failed to update attendance.";
+$lang['attendance_delete_success'] = "Attendance deleted successfully.";
+$lang['attendance_delete_fail']   = "Failed to delete attendance.";
+$lang['invalid_attendance_id']    = "Invalid attendance ID.";
+
+// Announcement
+$lang['announcement_add_success'] = "Announcement added successfully.";
+$lang['announcement_add_fail']    = "Failed to add announcement.";
+$lang['announcement_update_success'] = "Announcement updated successfully.";
+$lang['announcement_update_fail'] = "Failed to update announcement.";
+$lang['announcement_delete_success'] = "Announcement deleted successfully.";
+$lang['announcement_delete_fail'] = "Failed to delete announcement.";
+$lang['invalid_announcement_id']  = "Invalid announcement ID.";
+
+// Payment Method
+$lang['payment_method_add_success'] = "Payment method added successfully.";
+$lang['payment_method_add_fail']   = "Failed to add payment method.";
+$lang['payment_method_update_success'] = "Payment method updated successfully.";
+$lang['payment_method_update_fail'] = "Failed to update payment method.";
+$lang['payment_method_delete_success'] = "Payment method deleted successfully.";
+$lang['payment_method_delete_fail'] = "Failed to delete payment method.";
+$lang['invalid_payment_method_id'] = "Invalid payment method ID.";
+
+// User
+$lang['first_name_required']      = "First name is required.";
+$lang['last_name_required']       = "Last name is required.";
+$lang['email_required']           = "Email is required.";
+$lang['valid_email']              = "Please enter a valid email address.";
+$lang['user_type_required']       = "User type is required.";
+$lang['valid_user_type']          = "Invalid user type.";
+$lang['valid_gender']              = "Invalid gender.";
+$lang['valid_status']             = "Invalid status.";
+$lang['valid_client_id']          = "Invalid client ID.";
+$lang['valid_role_id']             = "Invalid role ID.";
+$lang['email_already_exists']     = "Email already exists.";
+$lang['user_add_success']         = "User added successfully.";
+$lang['user_add_fail']            = "Failed to add user.";
+$lang['user_update_success']      = "User updated successfully.";
+$lang['user_update_fail']         = "Failed to update user.";
+$lang['user_delete_success']      = "User deleted successfully.";
+$lang['user_delete_fail']         = "Failed to delete user.";
+$lang['invalid_user_id']          = "Invalid user ID.";
+
+// Task
+$lang['task_title_required']      = "Task title is required.";
+$lang['task_add_success']         = "Task added successfully.";
+$lang['task_add_fail']            = "Failed to add task.";
+$lang['task_update_success']      = "Task updated successfully.";
+$lang['task_update_fail']         = "Failed to update task.";
+$lang['task_delete_success']      = "Task deleted successfully.";
+$lang['task_delete_fail']         = "Failed to delete task.";
+$lang['invalid_task_id']          = "Invalid task ID.";
+$lang['project_id_required']      = "Project ID is required.";
+$lang['assigned_to_required']     = "Assigned to is required.";
+$lang['status_id_required']       = "Status ID is required.";
+$lang['invalid_status_id']        = "Invalid status ID.";
+$lang['priority_id_required']     = "Priority ID is required.";
+$lang['invalid_priority_id']      = "Invalid priority ID.";
+$lang['invalid_points']           = "Invalid points.";
+$lang['invalid_deadline']         = "Invalid deadline.";
+$lang['invalid_start_date']       = "Invalid start date.";
+
+// Expense
+$lang['expense_date_required']    = "Expense date is required.";
+$lang['invalid_expense_date']     = "Invalid expense date.";
+$lang['category_id_required']     = "Category ID is required.";
+$lang['invalid_category_id']      = "Invalid category ID.";
+$lang['expense_title_required']   = "Expense title is required.";
+$lang['amount_required']          = "Amount is required.";
+$lang['invalid_amount']           = "Invalid amount.";
+$lang['expense_add_success']      = "Expense added successfully.";
+$lang['expense_add_fail']         = "Failed to add expense.";
+$lang['expense_update_success']   = "Expense updated successfully.";
+$lang['expense_update_fail']      = "Failed to update expense.";
+$lang['expense_delete_success']   = "Expense deleted successfully.";
+$lang['expense_delete_fail']      = "Failed to delete expense.";
+$lang['invalid_expense_id']       = "Invalid expense ID.";
+
+// Activity Log
+$lang['invalid_log_id']           = "Invalid log ID.";
+
+return $lang;
