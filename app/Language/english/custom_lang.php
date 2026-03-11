96588752fe39a06cd729ee122280d7806eafa975
lanangwisana <lanangwisana@gmail.com>
2026-03-11 13:40:48 +0700

    feat(brain): update custom language

--- app/Language/english/custom_lang.php
+++ app/Language/english/custom_lang.php
@@ -3,5 +3,9 @@
 $lang["example"] = "Example";
 $lang["havia_cms"] = "Havia CMS";
 $lang["user_management"] = "User Management";
+$lang["add_user"] = "Add User";
+$lang["edit_staff"] = "Edit Staff";
+$lang["delete_user"] = "Delete User";
+$lang["is_admin"] = "Administrator Access";
 
 return $lang;
