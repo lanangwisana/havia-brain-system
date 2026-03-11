074ef524a7aed5751ad8e397f4f8e130c7ba16e5
lanangwisana <lanangwisana@gmail.com>
2026-03-11 12:21:14 +0700

    feat(deploy): HaviaCMS and API Sync stable version deployed

--- app/Language/english/custom_lang.php
+++ app/Language/english/custom_lang.php
@@ -1,5 +1,7 @@
 <?php //copy from default_lang.php file and update
 
 $lang["example"] = "Example";
+$lang["havia_cms"] = "Havia CMS";
+$lang["user_management"] = "User Management";
 
 return $lang;
